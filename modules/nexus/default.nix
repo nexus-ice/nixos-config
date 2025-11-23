f0977a4ae99165a076c63044f4c2004c2c31194b
nexus-ice <nexusice1234@googlemail.com>
2025-11-23 19:50:23 +0100

    neofetch nexus

--- modules/nexus/default.nix
+++ modules/nexus/default.nix
@@ -16,4 +16,5 @@
   	# Bootloader.
   	boot.loader.systemd-boot.enable = true;
   	boot.loader.efi.canTouchEfiVariables = true;
+
 }
--- modules/nexus/home.nix
+++ modules/nexus/home.nix
@@ -2,13 +2,16 @@
 {
 	home.username = "corum";
 	home.homeDirectory = "/home/corum";
-	home.packages = with pkgs; [ keepassxc github-desktop thunderbird ];
+	home.packages = with pkgs; [ keepassxc github-desktop thunderbird neofetch ];
 	dconf.settings = {
 		"org/gnome/desktop/interface" = {
 			color-scheme = "prefer-dark";
 		};
 	};
 	programs.firefox.enable = true;
+
+	programs.bash.enable = true;
+  	programs.bash.initExtra = ''${pkgs.neofetch}/bin/neofetch'';
 	
 	home.stateVersion = "25.05";
 }
