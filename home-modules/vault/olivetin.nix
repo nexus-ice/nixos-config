353ef32986c6a3f49e956b00bd4ffdcc481eaff4
nexus-ice <nexusice1234@googlemail.com>
2025-12-03 01:26:03 +0100

    a

--- home-modules/vault/olivetin.nix
+++ home-modules/vault/olivetin.nix
@@ -1,4 +1,4 @@
 { var , ... }:
 {
-	home.file."docker/olivetin/config/config.yaml".target = "nixos-config/dotfiles/${var.host}/olivetin/config.yaml";
+	home.file."docker/olivetin/config/config.yaml".target = "./nixos-config/dotfiles/${var.host}/olivetin/config.yaml";
 }
