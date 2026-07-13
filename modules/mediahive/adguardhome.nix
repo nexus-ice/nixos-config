2ea73abc06870f04bcffe589ea5a6bbfcda57140
nexus-ice <nexusice1234@googlemail.com>
2026-07-13 15:34:07 +0200

    adguard mist

--- modules/mediahive/adguardhome.nix
+++ modules/mediahive/adguardhome.nix
@@ -3,10 +3,10 @@
             enable = true;
             openFirewall = true;
             mutableSettings = true;
-            settings = {
-              http = {
-                address = "0.0.0.0:3000";
-              };
-            };
+            #settings = {
+            #  http = {
+            #    address = "0.0.0.0:3000";
+            #  };
+            #};
     };
 }
