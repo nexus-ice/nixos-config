b5c3cb81eeb53a97b31c4bb012fde864fe10c76f
nexus-ice <nexusice1234@googlemail.com>
2026-07-13 17:00:38 +0200

    test dns

--- modules/mediahive/adguardhome.nix
+++ modules/mediahive/adguardhome.nix
@@ -1,14 +1,18 @@
 { config, pkgs, var, ... }:{
     services.adguardhome = {
-            enable = true;
-            openFirewall = true;
-            mutableSettings = true;
-            #settings = {
-            #  http = {
-            #    address = "0.0.0.0:3000";
-            #  };
-            #};
+        enable = true;
+        openFirewall = true;
+        mutableSettings = true;
+        settings = {
+			http = {
+				address = "0.0.0.0:3000"; # Macht das Web-Interface im LAN erreichbar
+			};
+			dns = {
+				bind_hosts = [ "0.0.0.0" ]; # Lauscht auf allen Netzwerkschnittstellen nach DNS-Anfragen
+			};
+		};
     };
 	networking.firewall.allowedTCPPorts = [ 80 53 ];
+	networking.firewall.allowedUDPPorts = [ 53 ];
 	services.resolved.enable = false;
 }
