050d9f224840d63a8fc7e2bd37ebcb794d167a4f
nexus-ice <nexusice1234@googlemail.com>
2026-07-18 00:11:05 +0200

    navidome 2

---
+++ modules/mediahive/navidome.nix
@@ -0,0 +1,10 @@
+{
+	services.navidrome = {
+		enable = true;
+		openFirewall = true; # Öffnet Port 4533 automatisch
+		settings = {
+			MusicFolder = "/mnt/pool/Musik";
+			LogLevel = "info";
+		};
+	};
+}
