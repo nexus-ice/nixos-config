b197ab293fb35b490bd3c78610df70f1ca51332c
nexus-ice <nexusice1234@googlemail.com>
2026-07-23 02:07:42 +0200

    hidden share mediahive 2

--- modules/mediahive/samba_hidden_share.nix
+++ modules/mediahive/samba_hidden_share.nix
@@ -1,11 +1,9 @@
 { var , ... }:{
-	services.samba = { 
-		settings = {
-			cups$ = {
-					"path" = "/home/${var.user}/.hidden_share/";
-					"browseable" = "no";
-      				"writeable" = "yes";
-			};
+	services.samba.settings = {
+		"cups$" = {
+			"path" = "/home/${var.user}/.hidden_share/";
+			"browseable" = "no";
+      		"writeable" = "yes";
 		};
 	};
 }
